774aac9695d339df56ba0dfb656cf8d3b14e557b
Thorbjørn Hermansen <thhermansen@gmail.com>
2012-07-18 09:48:55 +0200

    Basic signature functionality in place.

--- lib/google_business_api_url_signer/signer.rb
+++ lib/google_business_api_url_signer/signer.rb
@@ -1,5 +1,40 @@
+require 'base64'
+require 'openssl'
+
 module GoogleBusinessApiUrlSigner
   class Signer
+    attr_reader :url, :private_key
 
+    def initialize(options = {})
+      @url = options.fetch :url
+      @private_key = options.fetch :private_key
+    end
+
+    def signature
+      Base64.encode64(signature_digest).tr('+/','-_').chomp
+    end
+
+
+    private
+
+    def signature_digest
+      OpenSSL::HMAC.digest(
+        OpenSSL::Digest.new('sha1'),
+        private_key_decoded, 
+        path_and_query
+      )
+    end
+
+    def parsed_url
+      URI(url)
+    end
+
+    def path_and_query
+      [parsed_url.path, parsed_url.query].join '?'
+    end
+
+    def private_key_decoded
+      Base64.decode64 private_key.tr('-_','+/')
+    end
   end
 end
--- spec/google_business_api_url_signer/signer_spec.rb
+++ spec/google_business_api_url_signer/signer_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe GoogleBusinessApiUrlSigner::Signer do
-  pending
+  let(:url) { "http://maps.googleapis.com/maps/api/geocode/json?address=New+York&sensor=false&client=clientID" }
+  let(:private_key) { "vNIXE0xscrmjlyV-12Nj_BvUPaw=" }
+  let(:signature) { "KrU1TzVQM7Ur0i8i7K3huiw3MsA=" }
+  let(:signed_url) { "http://maps.googleapis.com/maps/api/geocode/json?address=New+York&sensor=false&client=clientID&signature=#{signature}" }
+
+  subject do
+    described_class.new(
+      url: url,
+      private_key: private_key
+    )
+  end
+
+  its(:url) { should eq url }
+  its(:private_key) { should eq private_key }
+  its(:signature) { should eq signature }
 end
